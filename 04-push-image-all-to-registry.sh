099cee3caccb1551a2bd4675c7c02f2415c0da34
Shuichi Machida <shuichi.machida@gmail.com>
2022-05-31 22:47:56 +0900

    updated

--- 04-push-image-all-to-registry.sh
+++ 04-push-image-all-to-registry.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-HARBOR_HOST=172.16.140.11
+# change the following registry info
+ACCOUNT=smachida
 
-echo "pushing the images to the registry: $HARBOR_HOST"
-docker login $HARBOR_HOST
+echo "pushing the images to the dockerHub registry"
+docker login -u $ACCOUNT
 
-docker tag ms-demo-02-docker-product-service $HARBOR_HOST/ms-demo/ms-demo-02-docker-product-service
-docker push $HARBOR_HOST/ms-demo/ms-demo-02-docker-product-service
-docker tag ms-demo-02-docker-recommendation-service $HARBOR_HOST/ms-demo/ms-demo-02-docker-recommendation-service
-docker push $HARBOR_HOST/ms-demo/ms-demo-02-docker-recommendation-service
-docker tag ms-demo-02-docker-review-service $HARBOR_HOST/ms-demo/ms-demo-02-docker-review-service
-docker push $HARBOR_HOST/ms-demo/ms-demo-02-docker-review-service
-docker tag ms-demo-02-docker-product-composite-service $HARBOR_HOST/ms-demo/ms-demo-02-docker-product-composite-service
-docker push $HARBOR_HOST/ms-demo/ms-demo-02-docker-product-composite-service
+docker tag ms-demo-02-docker-product-service $ACCOUNT/ms-demo-02-docker-product-service
+docker push $ACCOUNT/ms-demo-02-docker-product-service
+docker tag ms-demo-02-docker-recommendation-service $ACCOUNT/ms-demo-02-docker-recommendation-service
+docker push $ACCOUNT/ms-demo-02-docker-recommendation-service
+docker tag ms-demo-02-docker-review-service $ACCOUNT/ms-demo-02-docker-review-service
+docker push $ACCOUNT/ms-demo-02-docker-review-service
+docker tag ms-demo-02-docker-product-composite-service $ACCOUNT/ms-demo-02-docker-product-composite-service
+docker push $ACCOUNT/ms-demo-02-docker-product-composite-service
